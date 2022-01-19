10f749b7fc240b4b77d7d3f8ba5889da36da4fb9
manikanta-1341 <medicharlamanikanta13@hmail.com>
2022-01-19 12:06:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var request=new XMLHttpRequest
+request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json")
+request.send()
+request.onload=function()
+{
+    var data=JSON.parse(request.response)
+    for(let i=0;i<data.length;i++)
+    {
+        //console.log(data)
+        console.log(`Country:${data[i].name} Flag:${data[i].flag}`)
+    }
+}
